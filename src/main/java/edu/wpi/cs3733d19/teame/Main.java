b9e742c9fccd51e9c5c3c5ead5c3bd7d6f4ad1f4
Elene Kavtaradze <ekavtaradze@wpi.edu>
2019-03-16 14:19:01 -0400

    elnee

--- src/main/java/edu/wpi/cs3733d19/teame/Main.java
+++ src/main/java/edu/wpi/cs3733d19/teame/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Connor Anderson");
         System.out.println("Carly P");
+        System.out.println("Elene Kavtaradze");
     }
 }
