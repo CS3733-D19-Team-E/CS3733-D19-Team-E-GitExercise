1ec468302f761011c9cce3a041014e609a2947bb
dashrives <danielshrives@gmail.com>
2019-03-16 13:50:31 -0400

    Added Name

--- src/main/java/edu/wpi/cs3733d19/teame/Main.java
+++ src/main/java/edu/wpi/cs3733d19/teame/Main.java
@@ -3,5 +3,6 @@ package edu.wpi.cs3733d19.teame;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Connor Anderson");
+        System.out.println("Daniel Shrives");
     }
 }
